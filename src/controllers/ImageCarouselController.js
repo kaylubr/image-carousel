8be8fab79f43d3a094d71a78b3e36614d774565f
kaylubr <kylebreyes092005@gmail.com>
2025-10-04 19:13:10 +0800

    Finish navigating images using dots and buttons

--- src/controllers/ImageCarouselController.js
+++ src/controllers/ImageCarouselController.js
@@ -1,39 +1,66 @@
-import ImageSlides from "../models/ImageSlides";
-
-function ImageCarouselController(){
-  const backBtn = document.querySelector('#left');
-  const forwardBtn = document.querySelector('#right');
-  const imageContainer = document.querySelector('#imageContainer');
-  const IMAGE_COUNT = 3;
-
-  let imageIndex = 1;
+class ImageCarousel {
+  static #IMAGE_COUNT = 3;
+  static #imageIndex = 0;
   
-  backBtn.addEventListener('click', () => {
-    resetImages();
-    if (imageIndex > 1) {
-      imageIndex -= 1;
+  static handleBackClick() {
+    ImageCarousel.#imageIndex -= 1;
+
+    if (ImageCarousel.#imageIndex < 0) {
+      ImageCarousel.#imageIndex = 0;
     }
 
-    const image = document.getElementById(`image${imageIndex}`);
-    image.style.display = 'block';
-  });
+   ImageCarousel.#renderImage();
+    ImageCarousel.handleNavDots();
+  }
 
-  forwardBtn.addEventListener('click', () => {
-    resetImages();
-    if (imageIndex < IMAGE_COUNT) {
-      imageIndex += 1;
+  static handleForwardClick() {
+    ImageCarousel.#resetImages();
+    ImageCarousel.#imageIndex += 1;
+
+    if (ImageCarousel.#imageIndex >= ImageCarousel.#IMAGE_COUNT) {
+      ImageCarousel.#imageIndex -= 1;
     }
 
-    const image = document.getElementById(`image${imageIndex}`);
-    image.style.display = 'block';
-  });
+    ImageCarousel.#renderImage();
+    ImageCarousel.handleNavDots();
+  }
+
+  static handleNavDotsClick(index) {
+    ImageCarousel.#imageIndex = index;
+    ImageCarousel.#renderImage();
+    ImageCarousel.handleNavDots();
+  }
+
+  static handleNavDots() {
+    ImageCarousel.#resetNavDots();
+    const currentDot = document.querySelector(`#nav${ImageCarousel.#imageIndex}`);
+    currentDot.style.backgroundColor = '#3e3e3e';
+  }
   
-  function resetImages() {
+  static #resetImages() {
     const allImages = document.querySelectorAll('#imageContainer > img');
     allImages.forEach(image => {
       image.style.display = 'none';
     });
   }
+
+  static #resetNavDots() {
+    const allDots = document.querySelectorAll('#imgNav > span');
+    allDots.forEach(dots => dots.style.backgroundColor = '#cacaca');
+  }
+  
+  static #renderImage() {
+    ImageCarousel.#resetImages();
+    const image = document.getElementById(`image${ImageCarousel.#imageIndex}`);
+    image.style.display = 'block';
+  }
+
+  static initialRender() {
+    const image = document.getElementById(`image0`);
+    const firstNavDot = document.querySelector('#imgNav > span:first-of-type');
+    image.style.display = 'block';
+    firstNavDot.style.backgroundColor = '#3e3e3e';
+  }
 }
 
-export default ImageCarouselController;
+export default ImageCarousel;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
-import ImageCarouselController from './controllers/ImageCarouselController';
+import ImageCarousel from './controllers/ImageCarouselController';
 import './style.css';
 
-ImageCarouselController();
+const forwardBtn = document.querySelector('#right');
+const backBtn = document.querySelector('#left');
+const navDots = document.querySelectorAll('#imgNav > span');
+
+backBtn.addEventListener('click', () => ImageCarousel.handleBackClick());
+forwardBtn.addEventListener('click', () => ImageCarousel.handleForwardClick());
+navDots.forEach(nav => {
+  nav.addEventListener('click', event => {
+    const index = parseInt(event.target.id.slice(3));
+    ImageCarousel.handleNavDotsClick(index);
+  });
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+  ImageCarousel.initialRender();
+})
+
